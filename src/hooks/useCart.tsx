afbb3fec981231a0ba156fe7dde6dfe4019a8de6
Helton Quintãns <helton.quit@gmail.com>
2022-03-24 18:20:50 -0300

    Logic of updateProductAmount finished

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -93,9 +93,33 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const updateCart = [...cart];
+      const prodctExists = updateCart.find(
+        (product) => product.id === productId
+      );
+
+      if (prodctExists) {
+        prodctExists.amount = amount;
+        setCart(updateCart);
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(updateCart));
+      } else {
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
